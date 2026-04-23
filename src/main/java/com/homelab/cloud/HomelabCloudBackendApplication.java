3c4f7422124e1b27943eab5e4adbfc13d7d9e0c7
SebastianFB28 <jahnsebastainfb28@gmail.com>
2026-04-22 22:03:01 -0500

    chore: initial Spring Boot project setup

---
+++ src/main/java/com/homelab/cloud/HomelabCloudBackendApplication.java
@@ -0,0 +1,13 @@
+package com.homelab.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomelabCloudBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomelabCloudBackendApplication.class, args);
+	}
+
+}
